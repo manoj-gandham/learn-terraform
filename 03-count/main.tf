30f2374554f2661f694d1198b7baa62bd13f85a8
manoj <gandhamanojkutty@gmail.com>
2023-11-26 00:04:04 +0530

    code updated

--- 03-count/main.tf
+++ 03-count/main.tf
@@ -2,14 +2,14 @@ resource "null_resource" "name" {
   count = 10
 }
 
-variable "names" {
-  default = [ "manoj", "ram","rama" ]
-}
-
 resource "null_resource" "names" {
   count = length(var.names)
 
   provisioner "local-exec" {
     command = "echo name - ${var.names[count.index]}"
   }
+}
+
+variable "names" {
+  default = [ "manoj", "ram","rama" ]
 }
