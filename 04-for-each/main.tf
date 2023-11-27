e2ab4cba373d30c305445c4031992f4208ed2f2e
manoj <gandhamanojkutty@gmail.com>
2023-11-27 14:20:19 +0530

    code updated

--- 04-for-each/main.tf
+++ 04-for-each/main.tf
@@ -1,13 +1,27 @@
-variable "name_of_the_colleges" {
+#variable "name_of_the_colleges" {
+#  default = {
+#    kphb = "JNTHU"
+#    bongloor = "AVNIT"
+#    mangapally = "SriIndu"
+#  }
+#}
+
+variable "list_of_colleges" {
   default = {
-    kphb = "JNTHU"
-    bongloor = "AVNIT"
-    mangapally = "SriIndu"
+    AVNIT = {
+      NAME = "MANOJ"
+    }
+    SRINIDU ={
+      NAME = "RAVI"
+    }
+    GURU = {
+      NAME = "SRIKANTH"
+    }
   }
 }
 
 resource "null_resource" "Collageis_names" {
-  for_each = var.name_of_the_colleges
+  for_each = var.list_of_colleges
   provisioner "local-exec" {
     command = "echo ${each.value} from ${each.key}"
   }
