a1985f503f41ecf86d09b5c03e0c57b80adfaad8
manoj <gandhamanojkutty@gmail.com>
2023-11-27 14:02:48 +0530

    code updated

--- 04-for-each/main.tf
+++ 04-for-each/main.tf
@@ -1,7 +1,7 @@
 resource "null_resource" "name_of_the_dist" {
   for_each = var.dist_names
   provisioner "local-exec" {
-    command = "echo name of the dist is ${var.dist_names[count.index]}"
+    command = "echo name of the dist is ${each.key - ${each.value}}"
   }
 }
 variable "dist_names" {
