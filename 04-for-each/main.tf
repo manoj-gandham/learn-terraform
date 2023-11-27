dd0b68cb1d324b0e86e51fff962b491a0004e4a1
manoj <gandhamanojkutty@gmail.com>
2023-11-27 13:57:57 +0530

    code updated

--- 04-for-each/main.tf
+++ 04-for-each/main.tf
@@ -2,5 +2,9 @@ resource "null_resource" "name_of_the_dist" {
   for_each = var.dist_names
 }
 variable "dist_names" {
-  default = ["adb","mncl","knr"]
+  default = {
+    mncl = adb
+    knr = jgtl
+    kagaz = rkp
+  }
 }
