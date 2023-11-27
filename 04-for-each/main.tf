5ae329fd60daee7ea9615e2834e86378e9f036fe
manoj <gandhamanojkutty@gmail.com>
2023-11-27 14:01:24 +0530

    code updated

--- 04-for-each/main.tf
+++ 04-for-each/main.tf
@@ -1,5 +1,8 @@
 resource "null_resource" "name_of_the_dist" {
   for_each = var.dist_names
+  provisioner "local-exec" {
+    command = "echo name of the dist is ${var.dist_names[count.index]}"
+  }
 }
 variable "dist_names" {
   default = {
