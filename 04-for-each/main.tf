5183f070fffb2a8129cbfb2c2f79c869914941b8
manoj <gandhamanojkutty@gmail.com>
2023-11-27 13:59:05 +0530

    code updated

--- 04-for-each/main.tf
+++ 04-for-each/main.tf
@@ -3,8 +3,8 @@ resource "null_resource" "name_of_the_dist" {
 }
 variable "dist_names" {
   default = {
-    mncl = adb
-    knr = jgtl
-    kagaz = rkp
+    mncl = "abd"
+    knr = "jgtl"
+    kagaz = "rkp"
   }
 }
