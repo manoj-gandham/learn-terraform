bba2b47b3da819ebc90b897f9e4ca857d6138e6c
manoj <gandhamanojkutty@gmail.com>
2024-01-27 11:40:35 +0530

    code updated

--- ex-module/ec2/main.tf
+++ ex-module/ec2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "instance" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
-  vpc_security_group_ids = var.sg_id
+  vpc_security_group_ids = [var.sg_id]
 
   tags = {
     Name = "test"
