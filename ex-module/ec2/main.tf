6f2724f0fad34687c8784707a599ecc2686bc73f
manoj <gandhamanojkutty@gmail.com>
2024-01-27 11:55:18 +0530

    code updated

--- ex-module/ec2/main.tf
+++ ex-module/ec2/main.tf
@@ -2,7 +2,6 @@ resource "aws_instance" "instance" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
   vpc_security_group_ids = [var.sg_id]
-  subnet_id = [var.sub_net]
 
   tags = {
     Name = "test"
@@ -16,4 +15,3 @@ data "aws_ami" "centos" {
 }
 
 variable "sg_id" {}
-variable "sub_net" {}
--- ex-module/main.tf
+++ ex-module/main.tf
@@ -1,13 +1,8 @@
 module "ec2" {
   source = "./ec2"
   sg_id = module.sg.sg_id
-  subnet = module.sub_net.subnet
 }
 
 module "sg" {
   source = "./sg"
-}
-
-module "sub_net" {
-  source = "./subnet"
 }
